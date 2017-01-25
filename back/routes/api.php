b17f76e007399c8d821aa2281510b950a8cd5ca9
leopard28 <muhamad.hendri.febriansyah14@mhs.if.its.ac.id>
2017-01-25 17:53:37 +0700

    Added laravel and dependency

---
+++ back/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('/test', function() {
+	return response()->json([
+		'user' => [
+			'name' => 'Hendri Fe'
+		]
+	]);
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+	Route::resource('products', 'ProductsController');	
+});
